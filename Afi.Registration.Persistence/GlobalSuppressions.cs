1489c452ffef028c74d2c5b98a1b7d710acdfd75
ne1410s <pgj646@gmail.com>
2021-10-04 09:44:02 +0100

    Add database migration

---
+++ Afi.Registration.Persistence/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+    "Formatting",
+    "RCS0056:A line is too long.",
+    Justification = "Auto generated migration code; typically lengthy",
+    Scope = "namespaceanddescendants",
+    Target = "~N:Afi.Registration.Persistence.Migrations")]
---
+++ Afi.Registration.Persistence/Migrations/20211004084155_InitialCreate.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Afi.Registration.Persistence.Migrations
+{
+    /// <summary>
+    /// The initial create migration.
+    /// </summary>
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc/>
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Forename = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Surname = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Policies",
+                columns: table => new
+                {
+                    PolicyId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PolicyReference = table.Column<string>(type: "TEXT", maxLength: 16, nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ActivatedOn = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    OwnerId = table.Column<long>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Policies", x => x.PolicyId);
+                    table.UniqueConstraint("AK_Policies_PolicyReference", x => x.PolicyReference);
+                    table.ForeignKey(
+                        name: "FK_Policies_Customers_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Email",
+                table: "Customers",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Policies_OwnerId",
+                table: "Policies",
+                column: "OwnerId");
+        }
+
+        /// <inheritdoc/>
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Policies");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
